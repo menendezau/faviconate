744ed0552f8e6ace5c97f81b928c78ca6b05a105
Jose <menendez@autonomic.ai>
2018-10-01 18:49:40 -0700

    LinearIcons added

---
+++ src/linearicon.ts
@@ -0,0 +1,195 @@
+import {ui} from "./ui";
+
+export namespace linearicon{
+
+
+    import IconItem = ui.IconItem;
+
+    export class LinearIcon extends IconItem{
+
+
+        private static makeFrom(className: string): LinearIcon{
+
+
+
+        }
+
+        static get home                (): LinearIcon{ return this.makeFrom("home");                     }
+        static get apartment           (): LinearIcon{ return this.makeFrom("apartment");                }
+        static get pencil              (): LinearIcon{ return this.makeFrom("pencil");                   }
+        static get magicWand           (): LinearIcon{ return this.makeFrom("magic-wand");               }
+        static get drop                (): LinearIcon{ return this.makeFrom("drop");                     }
+        static get lighter             (): LinearIcon{ return this.makeFrom("lighter");                  }
+        static get poop                (): LinearIcon{ return this.makeFrom("poop");                     }
+        static get sun                 (): LinearIcon{ return this.makeFrom("sun");                      }
+        static get moon                (): LinearIcon{ return this.makeFrom("moon");                     }
+        static get cloud               (): LinearIcon{ return this.makeFrom("cloud");                    }
+        static get cloudUpload         (): LinearIcon{ return this.makeFrom("cloud-upload");             }
+        static get cloudDownload       (): LinearIcon{ return this.makeFrom("cloud-download");           }
+        static get cloudSync           (): LinearIcon{ return this.makeFrom("cloud-sync");               }
+        static get cloudCheck          (): LinearIcon{ return this.makeFrom("cloud-check");              }
+        static get database            (): LinearIcon{ return this.makeFrom("database");                 }
+        static get lock                (): LinearIcon{ return this.makeFrom("lock");                     }
+        static get cog                 (): LinearIcon{ return this.makeFrom("cog");                      }
+        static get trash               (): LinearIcon{ return this.makeFrom("trash");                    }
+        static get dice                (): LinearIcon{ return this.makeFrom("dice");                     }
+        static get heart               (): LinearIcon{ return this.makeFrom("heart");                    }
+        static get star                (): LinearIcon{ return this.makeFrom("star");                     }
+        static get starHalf            (): LinearIcon{ return this.makeFrom("star-half");                }
+        static get starEmpty           (): LinearIcon{ return this.makeFrom("star-empty");               }
+        static get flag                (): LinearIcon{ return this.makeFrom("flag");                     }
+        static get envelope            (): LinearIcon{ return this.makeFrom("envelope");                 }
+        static get paperclip           (): LinearIcon{ return this.makeFrom("paperclip");                }
+        static get inbox               (): LinearIcon{ return this.makeFrom("inbox");                    }
+        static get eye                 (): LinearIcon{ return this.makeFrom("eye");                      }
+        static get printer             (): LinearIcon{ return this.makeFrom("printer");                  }
+        static get fileEmpty           (): LinearIcon{ return this.makeFrom("file-empty");               }
+        static get fileAdd             (): LinearIcon{ return this.makeFrom("file-add");                 }
+        static get enter               (): LinearIcon{ return this.makeFrom("enter");                    }
+        static get exit                (): LinearIcon{ return this.makeFrom("exit");                     }
+        static get graduationHat       (): LinearIcon{ return this.makeFrom("graduation-hat");           }
+        static get license             (): LinearIcon{ return this.makeFrom("license");                  }
+        static get musicNote           (): LinearIcon{ return this.makeFrom("music-note");               }
+        static get filmPlay            (): LinearIcon{ return this.makeFrom("film-play");                }
+        static get cameraVideo         (): LinearIcon{ return this.makeFrom("camera-video");             }
+        static get camera              (): LinearIcon{ return this.makeFrom("camera");                   }
+        static get picture             (): LinearIcon{ return this.makeFrom("picture");                  }
+        static get book                (): LinearIcon{ return this.makeFrom("book");                     }
+        static get bookmark            (): LinearIcon{ return this.makeFrom("bookmark");                 }
+        static get user                (): LinearIcon{ return this.makeFrom("user");                     }
+        static get users               (): LinearIcon{ return this.makeFrom("users");                    }
+        static get shirt               (): LinearIcon{ return this.makeFrom("shirt");                    }
+        static get store               (): LinearIcon{ return this.makeFrom("store");                    }
+        static get cart                (): LinearIcon{ return this.makeFrom("cart");                     }
+        static get tag                 (): LinearIcon{ return this.makeFrom("tag");                      }
+        static get phoneHandset        (): LinearIcon{ return this.makeFrom("phone-handset");            }
+        static get phone               (): LinearIcon{ return this.makeFrom("phone");                    }
+        static get pushpin             (): LinearIcon{ return this.makeFrom("pushpin");                  }
+        static get mapMarker           (): LinearIcon{ return this.makeFrom("map-marker");               }
+        static get map                 (): LinearIcon{ return this.makeFrom("map");                      }
+        static get location            (): LinearIcon{ return this.makeFrom("location");                 }
+        static get calendarFull        (): LinearIcon{ return this.makeFrom("calendar-full");            }
+        static get keyboard            (): LinearIcon{ return this.makeFrom("keyboard");                 }
+        static get spellCheck          (): LinearIcon{ return this.makeFrom("spell-check");              }
+        static get screen              (): LinearIcon{ return this.makeFrom("screen");                   }
+        static get smartphone          (): LinearIcon{ return this.makeFrom("smartphone");               }
+        static get tablet              (): LinearIcon{ return this.makeFrom("tablet");                   }
+        static get laptop              (): LinearIcon{ return this.makeFrom("laptop");                   }
+        static get laptopPhone         (): LinearIcon{ return this.makeFrom("laptop-phone");             }
+        static get powerSwitch         (): LinearIcon{ return this.makeFrom("power-switch");             }
+        static get bubble              (): LinearIcon{ return this.makeFrom("bubble");                   }
+        static get heartPulse          (): LinearIcon{ return this.makeFrom("heart-pulse");              }
+        static get construction        (): LinearIcon{ return this.makeFrom("construction");             }
+        static get pieChart            (): LinearIcon{ return this.makeFrom("pie-chart");                }
+        static get chartBars           (): LinearIcon{ return this.makeFrom("chart-bars");               }
+        static get gift                (): LinearIcon{ return this.makeFrom("gift");                     }
+        static get diamond             (): LinearIcon{ return this.makeFrom("diamond");                  }
+        static get linearicons         (): LinearIcon{ return this.makeFrom("linearicons");              }
+        static get dinner              (): LinearIcon{ return this.makeFrom("dinner");                   }
+        static get coffeeCup           (): LinearIcon{ return this.makeFrom("coffee-cup");               }
+        static get leaf                (): LinearIcon{ return this.makeFrom("leaf");                     }
+        static get paw                 (): LinearIcon{ return this.makeFrom("paw");                      }
+        static get rocket              (): LinearIcon{ return this.makeFrom("rocket");                   }
+        static get briefcase           (): LinearIcon{ return this.makeFrom("briefcase");                }
+        static get bus                 (): LinearIcon{ return this.makeFrom("bus");                      }
+        static get car                 (): LinearIcon{ return this.makeFrom("car");                      }
+        static get train               (): LinearIcon{ return this.makeFrom("train");                    }
+        static get bicycle             (): LinearIcon{ return this.makeFrom("bicycle");                  }
+        static get wheelchair          (): LinearIcon{ return this.makeFrom("wheelchair");               }
+        static get select              (): LinearIcon{ return this.makeFrom("select");                   }
+        static get earth               (): LinearIcon{ return this.makeFrom("earth");                    }
+        static get smile               (): LinearIcon{ return this.makeFrom("smile");                    }
+        static get sad                 (): LinearIcon{ return this.makeFrom("sad");                      }
+        static get neutral             (): LinearIcon{ return this.makeFrom("neutral");                  }
+        static get mustache            (): LinearIcon{ return this.makeFrom("mustache");                 }
+        static get alarm               (): LinearIcon{ return this.makeFrom("alarm");                    }
+        static get bullhorn            (): LinearIcon{ return this.makeFrom("bullhorn");                 }
+        static get volumeHigh          (): LinearIcon{ return this.makeFrom("volume-high");              }
+        static get volumeMedium        (): LinearIcon{ return this.makeFrom("volume-medium");            }
+        static get volumeLow           (): LinearIcon{ return this.makeFrom("volume-low");               }
+        static get volume              (): LinearIcon{ return this.makeFrom("volume");                   }
+        static get mic                 (): LinearIcon{ return this.makeFrom("mic");                      }
+        static get hourglass           (): LinearIcon{ return this.makeFrom("hourglass");                }
+        static get undo                (): LinearIcon{ return this.makeFrom("undo");                     }
+        static get redo                (): LinearIcon{ return this.makeFrom("redo");                     }
+        static get sync                (): LinearIcon{ return this.makeFrom("sync");                     }
+        static get history             (): LinearIcon{ return this.makeFrom("history");                  }
+        static get clock               (): LinearIcon{ return this.makeFrom("clock");                    }
+        static get download            (): LinearIcon{ return this.makeFrom("download");                 }
+        static get upload              (): LinearIcon{ return this.makeFrom("upload");                   }
+        static get enterDown           (): LinearIcon{ return this.makeFrom("enter-down");               }
+        static get exitUp              (): LinearIcon{ return this.makeFrom("exit-up");                  }
+        static get bug                 (): LinearIcon{ return this.makeFrom("bug");                      }
+        static get code                (): LinearIcon{ return this.makeFrom("code");                     }
+        static get link                (): LinearIcon{ return this.makeFrom("link");                     }
+        static get unlink              (): LinearIcon{ return this.makeFrom("unlink");                   }
+        static get thumbsUp            (): LinearIcon{ return this.makeFrom("thumbs-up");                }
+        static get thumbsDown          (): LinearIcon{ return this.makeFrom("thumbs-down");              }
+        static get magnifier           (): LinearIcon{ return this.makeFrom("magnifier");                }
+        static get cross               (): LinearIcon{ return this.makeFrom("cross");                    }
+        static get menu                (): LinearIcon{ return this.makeFrom("menu");                     }
+        static get list                (): LinearIcon{ return this.makeFrom("list");                     }
+        static get chevronUp           (): LinearIcon{ return this.makeFrom("chevron-up");               }
+        static get chevronDown         (): LinearIcon{ return this.makeFrom("chevron-down");             }
+        static get chevronLeft         (): LinearIcon{ return this.makeFrom("chevron-left");             }
+        static get chevronRight        (): LinearIcon{ return this.makeFrom("chevron-right");            }
+        static get arrowUp             (): LinearIcon{ return this.makeFrom("arrow-up");                 }
+        static get arrowDown           (): LinearIcon{ return this.makeFrom("arrow-down");               }
+        static get arrowLeft           (): LinearIcon{ return this.makeFrom("arrow-left");               }
+        static get arrowRight          (): LinearIcon{ return this.makeFrom("arrow-right");              }
+        static get move                (): LinearIcon{ return this.makeFrom("move");                     }
+        static get warning             (): LinearIcon{ return this.makeFrom("warning");                  }
+        static get questionCircle      (): LinearIcon{ return this.makeFrom("question-circle");          }
+        static get menuCircle          (): LinearIcon{ return this.makeFrom("menu-circle");              }
+        static get checkmarkCircle     (): LinearIcon{ return this.makeFrom("checkmark-circle");         }
+        static get crossCircle         (): LinearIcon{ return this.makeFrom("cross-circle");             }
+        static get plusCircle          (): LinearIcon{ return this.makeFrom("plus-circle");              }
+        static get circleMinus         (): LinearIcon{ return this.makeFrom("circle-minus");             }
+        static get arrowUpCircle       (): LinearIcon{ return this.makeFrom("arrow-up-circle");          }
+        static get arrowDownCircle     (): LinearIcon{ return this.makeFrom("arrow-down-circle");        }
+        static get arrowLeftCircle     (): LinearIcon{ return this.makeFrom("arrow-left-circle");        }
+        static get arrowRightCircle    (): LinearIcon{ return this.makeFrom("arrow-right-circle");       }
+        static get chevronUpCircle     (): LinearIcon{ return this.makeFrom("chevron-up-circle");        }
+        static get chevronDownCircle   (): LinearIcon{ return this.makeFrom("chevron-down-circle");      }
+        static get chevronLeftCircle   (): LinearIcon{ return this.makeFrom("chevron-left-circle");      }
+        static get chevronRightCircle  (): LinearIcon{ return this.makeFrom("chevron-right-circle");     }
+        static get crop                (): LinearIcon{ return this.makeFrom("crop");                     }
+        static get frameExpand         (): LinearIcon{ return this.makeFrom("frame-expand");             }
+        static get frameContract       (): LinearIcon{ return this.makeFrom("frame-contract");           }
+        static get layers              (): LinearIcon{ return this.makeFrom("layers");                   }
+        static get funnel              (): LinearIcon{ return this.makeFrom("funnel");                   }
+        static get textFormat          (): LinearIcon{ return this.makeFrom("text-format");              }
+        static get textFormatRemove    (): LinearIcon{ return this.makeFrom("text-format-remove");       }
+        static get textSize            (): LinearIcon{ return this.makeFrom("text-size");                }
+        static get bold                (): LinearIcon{ return this.makeFrom("bold");                     }
+        static get italic              (): LinearIcon{ return this.makeFrom("italic");                   }
+        static get underline           (): LinearIcon{ return this.makeFrom("underline");                }
+        static get strikethrough       (): LinearIcon{ return this.makeFrom("strikethrough");            }
+        static get highlight           (): LinearIcon{ return this.makeFrom("highlight");                }
+        static get textAlignLeft       (): LinearIcon{ return this.makeFrom("text-align-left");          }
+        static get textAlignCenter     (): LinearIcon{ return this.makeFrom("text-align-center");        }
+        static get textAlignRight      (): LinearIcon{ return this.makeFrom("text-align-right");         }
+        static get textAlignJustify    (): LinearIcon{ return this.makeFrom("text-align-justify");       }
+        static get lineSpacing         (): LinearIcon{ return this.makeFrom("line-spacing");             }
+        static get indentIncrease      (): LinearIcon{ return this.makeFrom("indent-increase");          }
+        static get indentDecrease      (): LinearIcon{ return this.makeFrom("indent-decrease");          }
+        static get pilcrow             (): LinearIcon{ return this.makeFrom("pilcrow");                  }
+        static get directionLtr        (): LinearIcon{ return this.makeFrom("direction-ltr");            }
+        static get directionRtl        (): LinearIcon{ return this.makeFrom("direction-rtl");            }
+        static get pageBreak           (): LinearIcon{ return this.makeFrom("page-break");               }
+        static get sortAlphaAsc        (): LinearIcon{ return this.makeFrom("sort-alpha-asc");           }
+        static get sortAmountAsc       (): LinearIcon{ return this.makeFrom("sort-amount-asc");          }
+        static get hand                (): LinearIcon{ return this.makeFrom("hand");                     }
+        static get pointerUp           (): LinearIcon{ return this.makeFrom("pointer-up");               }
+        static get pointerRight        (): LinearIcon{ return this.makeFrom("pointer-right");            }
+        static get pointerDown         (): LinearIcon{ return this.makeFrom("pointer-down");             }
+        static get pointerLeft         (): LinearIcon{ return this.makeFrom("pointer-left");             }
+
+        constructor(){
+            super();
+        }
+
+    }
+
+
+}
